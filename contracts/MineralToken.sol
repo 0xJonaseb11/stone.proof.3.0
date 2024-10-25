f75f99f4ee53539664ab1225a934fc0551b4d1a6
0xJonaseb11 <sebejaz99@gmail.com>
2024-10-25 16:26:44 +0200

    update fix #counter

--- contracts/MineralToken.sol
+++ contracts/MineralToken.sol
@@ -2,14 +2,30 @@
 pragma solidity ^0.8.20;
 
 /**
-* @contract MineraToken 
+* contract MineraToken 
+* @author: @0xJonaseb11
 * @notice A utility token(NFT) for digital certification of minerals
 * @param Could represent a batch of minerals, each token tracking a certain quantity or type of mineral
 * @param Integrates with the Supplychain.sol for transferring mineral ownership
 */
 
-contract MineralToken {
-    // states
+// imports
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
+import { Counters } from "@openzeppelin/contracts/utils/Counters.sol";
+
+
+
+contract MineralToken  is ERC721, AccessControl {
+    // states 
+    using Counters for counters.Counter;
+
+    Counters.Counter private _tokenIds;
+    bytes32 public constant MINTER_ROLE =  keccak256("MINTER_ROLE");
+    bytes32 public constant TRANSFER_ROLE = keccak256("TRANSFER_ROLE");
+
+    // struct 
+
 
     // events
 
