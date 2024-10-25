f3c65e371f92aace5901bc63483ecf912c68a728
0xJonaseb11 <sebejaz99@gmail.com>
2024-10-25 17:02:00 +0200

    update init mintMineral #MineralToken

--- contracts/MineralToken.sol
+++ contracts/MineralToken.sol
@@ -18,22 +18,64 @@ import { Counters } from "@openzeppelin/contracts/utils/Counters.sol";
 
 contract MineralToken  is ERC721, AccessControl {
     // states 
+    // mapping to store batch details
+    mapping(uint256 => MineralBatch) public mineralBatches;
+    mapping(uint256 => bool) public verifiedBatches;
     using Counters for counters.Counter;
 
     Counters.Counter private _tokenIds;
     bytes32 public constant MINTER_ROLE =  keccak256("MINTER_ROLE");
     bytes32 public constant TRANSFER_ROLE = keccak256("TRANSFER_ROLE");
 
-    // struct 
-
+    // struct for mineral batch
+    struct MineralBatch {
+        uint256 batchId;
+        string origin;
+        string purity;
+        string metadataURI;
+        uint256 timestamp;
+        bool isCertified;
+    }
 
     // events
+    event MineralMinted(uint256 indexed tokenId, string origin, string purity, string metadataURI);
+    event MineralVerified(uint256 indexed tokenId,  bool isCertified);
+
+    // contract constructor
+    constructor() ERC721("MineralToken", "MTK") {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+
 
     // errors
 
     // business logic
 
-    // setters
+    // setters - cores
+    //mint mineral
+    function mintMineral(string memory _origin, string memory _purity, string memory _metadataURI) public onlyRole(MINTER_ROLE) returns(uint256) {
+        _tokenIds.increment();
+        uint256 newBatchId = _tokenIds.current();
+
+        // mint the new token representing the mineral batch
+        _mint(msg.sender, newBatchId);
+
+        // store batch details
+        mineralBatches[newBatchId] = MineralBatch({
+            batchId: newBatchId,
+            origin: _origin,
+            purity: _purity,
+            metadataURI: _metadataURI,
+            timestamp: block.timestamp,
+            isCertified: false
+        });
+
+        // log event to blockchain after minting minetal token
+        emit MineralMinted(newBatchId, _origin, _purity, _metadataURI);
+        // return targeted batchId(uint256)
+        return newBatchId;
+    }
 
     // getters
 }
