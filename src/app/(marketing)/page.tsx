4fa8c73084f7d0df74d6a7ca004378679042ea5e
Irere Emmy <irere2020@outlook.com>
2024-10-26 01:28:00 +0200

    feat: features cards with animated svgs

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,49 +1,54 @@
 "use client";
 
-import { motion } from "framer-motion";
+import {
+  AnimatedCircles,
+  HealthSvg,
+  LayeredCircles,
+  SwapSvgIcon,
+} from "@/components/svgs";
 
 export default function Page() {
   return (
     <div className="sm:py-28 py-10 sm:px-12">
-      <div>
+      <div className="mx-auto max-w-5xl">
         <h2 className="text-4xl font-medium">Trust, Unlocked.</h2>
         <div
-          className="grid grid-cols-2 mt-12"
+          className="grid grid-cols-2 mt-12 gap-10"
           style={{ gridTemplateRows: "300px 300px" }}
         >
-          <div className="flex flex-col justify-end p-8 rounded-xl relative">
-            <motion.svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 268 150"
-              initial={{ opacity: 0.9, scale: 1 }}
-              animate={{
-                opacity: [0.9, 1, 0.9],
-                scale: [1, 1.05, 1],
-              }}
-              transition={{
-                duration: 3.5,
-                repeat: Infinity,
-                ease: "easeInOut",
-              }}
-              className={`absolute top-0 right-0`}
-            >
-              <circle cx="70" cy="40" r="40" fill="#FF5722" />
-              <circle cx="130" cy="40" r="40" fill="#FF8A65" />
-            </motion.svg>
-            <p>Swap</p>
-            <p>Swap any ERC-20, even those borrowed or supplied.</p>
+          <div className="flex flex-col gap-3 justify-end p-8 rounded-xl relative">
+            <SwapSvgIcon />
+            <p className="text-xl font-medium text-neutral-800">Swap</p>
+            <p className="text-neutral-600">
+              Swap any ERC-20, even those borrowed or supplied.
+            </p>
           </div>
-          <div className="p-8">
-            <p>Stake</p>
-            <p>Earn rewards for securing the protocol</p>
+          <div className="flex flex-col gap-3 justify-end p-8 rounded-xl relative">
+            <AnimatedCircles />
+            <p className="text-xl font-medium text-neutral-800">Stake</p>
+            <p className="text-neutral-600">
+              Earn rewards for securing the protocol
+            </p>
           </div>
-          <div className="p-8">
-            <p>Health factor</p>
-            <p>Easily track the risk level of your borrow positions.</p>
+          <div className="flex flex-col gap-3 justify-end p-8 rounded-xl relative">
+            <HealthSvg />
+            <p className="text-xl font-medium text-neutral-800">
+              Health factor
+            </p>
+            <p className="text-neutral-600">
+              Easily track the risk level of your borrow positions.
+            </p>
           </div>
-          <div className="p-8">
-            <p>Multi-Network</p>
-            <p>Deployable on any EVM compatible network.</p>
+          <div className="flex flex-col gap-3 justify-end p-8 rounded-xl relative">
+            <div>
+              <LayeredCircles />
+            </div>
+            <p className="text-xl font-medium text-neutral-800">
+              Multi-Network
+            </p>
+            <p className="text-neutral-600">
+              Deployable on any EVM compatible network.
+            </p>
           </div>
         </div>
       </div>
---
+++ src/components/svgs/index.ts
@@ -0,0 +1,4 @@
+export { default as SwapSvgIcon } from "./swap-svg";
+export { default as AnimatedCircles } from "./animated-circles";
+export { default as LayeredCircles } from "./layered-circles";
+export { default as HealthSvg } from "./health-svg";
