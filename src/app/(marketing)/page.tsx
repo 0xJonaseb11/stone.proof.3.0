50d1a645ff33d1fb2e712074d8fb9fc2b45c3438
Irere123 <irere2020@outlook.com>
2024-10-25 23:32:04 +0200

    changes

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,3 +1,7 @@
 export default function Page() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="py-32">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    </div>
+  );
 }
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,31 @@
+import Image from "next/image";
+
 export const Footer: React.FC = () => {
   return (
-    <div>
-      <p>Hello world</p>
-    </div>
+    <footer className="grid grid-cols-2 gap-4 justify-items-stretch p-8 mx-auto max-w-5xl bg-neutral-100 mb-4 rounded-xl">
+      <div className="space-y-3">
+        <Image
+          src={`/logo_dark.svg`}
+          alt="Stoneproof"
+          width={150}
+          height={38}
+        />
+        <p className="text-xs">
+          stone.proof.com provides information and resources about the
+          fundamentals of the decentralised non-custodial liquidity protocol
+          called the Stoneproof Protocol, comprised of open-source
+          self-executing smart contracts that are deployed on various
+          permissionless public blockchains.
+        </p>
+      </div>
+      <div className="flex flex-col items-end space-y-2">
+        <h3 className="text-xl text-neutral-800">
+          Stay up to date with Stone.proof
+        </h3>
+        <p className="text-sm text-neutral-600">
+          Subscribe to the newsletter for updates
+        </p>
+      </div>
+    </footer>
   );
 };
